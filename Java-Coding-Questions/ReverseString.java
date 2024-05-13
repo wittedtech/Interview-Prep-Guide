2fc91d4dd9d2e9d96dd05446939877d063fd9655
Harshit Singh <ceh.harshit@gmail.com>
2024-05-14 00:33:03 +0530

    Coding Questions
    Question Given in Agnisys.
    
    Reverse String including its word to be reversed . Both the Files are for teh same question solution just with two different approach.
    
    Can also solve this using XOR Operator to reverse words without using and third variable.
    
    static String reversingString(char []str,
                                   int start,
                                   int end)
    {
        // Iterate loop upto start not equal to end
        while (start < end)
        {
            // XOR for swapping the variable
            str[start] ^= str[end];
            str[end] ^= str[start];
            str[start] ^= str[end];
    
            ++start;
            --end;
        }
        return String.valueOf(str);
    }
    
    XOR returns 1 when both values are different otherwise 0.

---
+++ Java-Coding-Questions/ReverseString.java
@@ -0,0 +1,30 @@
+class ReverseString{
+    public static void main(String[] args) {
+        String sentence = "The Quick Brown Fox Jump over the Cat";
+        String reversed = reverseWords(sentence);
+        System.out.println(reversed);
+    }
+
+    public static String reverseWords(String sentenceString) {
+        String [] words = sentenceString.split(" ");
+        StringBuilder reversedSentenceString = new StringBuilder();
+        for(int i=words.length-1; i>=0; i--) {
+            String reversedWord = reverseEveryWord(words[i]);
+            reversedSentenceString.append(reversedWord +" ");
+        }
+        return reversedSentenceString.toString().trim();
+    }
+    public static String reverseEveryWord(String words){
+        char [] charArray = words.toCharArray();
+        int left = 0;
+        int right = charArray.length-1;
+        while( left < right){
+            char temp = charArray[left];
+            charArray[left] = charArray[right];
+            charArray[right] = temp;
+            left++;  
+            right--;
+        }
+        return new String(charArray);
+    }
+} 
---
+++ Java-Coding-Questions/ReverseString1.java
@@ -0,0 +1,13 @@
+public class ReverseString1{
+    public static void main(String [] args){
+        String str = "The Quick Brown Fox Jump Over";
+        String str1 = ("");
+
+        for(int i=0 ; i<str.length(); i++){
+            char ch = str.charAt(i);
+            str1 = ch+str1;
+        }
+
+        System.out.println(str1);
+    }
+}
